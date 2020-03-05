74ce8809e1e12d321eb1990dd0468acf53eb0b68
Marten Meinhardt <marten.meinhardt@stud.hs-hannover.de>
2020-03-05 13:22:49 +0100

    started with authentication

---
+++ dashpy/authentication/Authenticator.py
@@ -0,0 +1,20 @@
+from hashlib import sha384
+from os.path import expanduser
+import json
+
+
+def authenticate(passphrase, hwtoken):
+    homedir = expanduser('~')
+    try:
+        with open(homedir + '/.dashpy/authentication.json') as json_file:
+            validation_data = json.load(json_file)
+            print(validation_data)
+    except Exception:
+        print("DashPy could not locate the authentication file.")
+
+
+def main():
+    authenticate("abc123", "ichbineinhwtoken")
+
+if __name__ == '__main__':
+    main()
