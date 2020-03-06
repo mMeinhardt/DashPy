9117382a3da72d6317b28a02e7346045b7234b01
Marten Meinhardt <marten.meinhardt@stud.hs-hannover.de>
2020-03-06 09:13:35 +0100

    added authentication credentials deletion in auth function

--- dashpy/authentication/Authenticator.py
+++ dashpy/authentication/Authenticator.py
@@ -12,8 +12,11 @@ def authenticate(password, hwtoken):
         print("DashPy could not locate the authentication file. Make sure it is located under ~/.dashpy")
     password_hash = sha384()
     password_hash.update((password + validation_data["PWSalt"]).encode('utf-8'))
+    del password
     hwtoken_hash = sha384()
     hwtoken_hash.update(hwtoken.encode('utf-8'))
+    del hwtoken
     if((password_hash.hexdigest() == validation_data["Password"]) and (hwtoken_hash.hexdigest() == validation_data["HWToken"])):
         return True
     return False
+
