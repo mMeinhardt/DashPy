fe0c09e076a6774981be859174e3f981ae2c46a9
Marten Meinhardt <marten.meinhardt@stud.hs-hannover.de>
2020-03-06 09:08:08 +0100

    Authentication done

--- dashpy/authentication/Authenticator.py
+++ dashpy/authentication/Authenticator.py
@@ -3,21 +3,17 @@ from os.path import expanduser
 import json
 
 
-def authenticate(passphrase, hwtoken):
+def authenticate(password, hwtoken):
     homedir = expanduser('~')
     try:
         with open(homedir + '/.dashpy/authentication.json') as json_file:
             validation_data = json.load(json_file)
     except Exception:
         print("DashPy could not locate the authentication file. Make sure it is located under ~/.dashpy")
-    passphrase_hash = sha384()
-    passphrase_hash.update((passphrase + validation_data["PWSalt"]).encode('utf-8'))
+    password_hash = sha384()
+    password_hash.update((password + validation_data["PWSalt"]).encode('utf-8'))
     hwtoken_hash = sha384()
     hwtoken_hash.update(hwtoken.encode('utf-8'))
-
-
-def main():
-    authenticate("123qweasd", "dasistderhashvomhwtoken")
-
-if __name__ == '__main__':
-    main()
+    if((password_hash.hexdigest() == validation_data["Password"]) and (hwtoken_hash.hexdigest() == validation_data["HWToken"])):
+        return True
+    return False
