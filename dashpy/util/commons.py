76fa7743ed61fc146e6c1a3c105aa0a3307a4c60
Marten Meinhardt <marten.meinhardt@stud.hs-hannover.de>
2020-03-12 14:06:02 +0100

    added salt function

--- dashpy/util/commons.py
+++ dashpy/util/commons.py
@@ -1,2 +1,4 @@
 AMOUNTWORDS = 10
 SIZEOFFILLBYTES = 10
+# The length of the salt in bytes
+SALTLENGTH = 10
--- dashpy/util/crypto_util.py
+++ dashpy/util/crypto_util.py
@@ -1,6 +1,8 @@
 # module containing different crypto modules such instant-digest of cryptographic hash functions
 
 import hashlib as hash
+import secrets
+import dashpy.util.commons as commons
 
 def get_sha384_hex(input):
     hasher = hash.sha384()
@@ -11,3 +13,6 @@ def get_sha256_hex(input):
     hasher = hash.sha3_256()
     hasher.update(input.encode('utf-8'))
     return hasher.hexdigest()
+
+def create_salt():
+    return secrets.token_hex(commons.SALTLENGTH)
